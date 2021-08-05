abd4a0612ec465f19d193f66528d74f4f5f35427
Nilesh <Nilesh@LAPTOP-22R4V3MJ>
2021-08-05 18:03:38 +0530

    [nilesh]:demo2 has been created for project

---
+++ src/sample/demo2.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class demo2 {
+	public static void main(String[] args) {
+		System.out.println("hello world ");
+		System.out.println("hello nagesh ");
+
+
+		
+		
+		
+	}
+
+}
